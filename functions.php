28d0965e434a8a2a88cae98b7c73c7c02a51d9f2
thiago.coelho <thiago.coelho@sltools.com.br>
2026-08-31 13:58:32 -0300

    feat: adiciona cadastro de caes no WordPress

--- functions.php
+++ functions.php
@@ -39,3 +39,54 @@ function kadoshdogs_assets()
 }
 
 add_action('wp_enqueue_scripts', 'kadoshdogs_assets');
+
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Registra o tipo de conteúdo "Cães"
+ */
+function kadoshdogs_register_dogs_post_type()
+{
+    $labels = [
+        'name'               => 'Cães',
+        'singular_name'      => 'Cão',
+        'menu_name'          => 'Cães',
+        'name_admin_bar'     => 'Cão',
+        'add_new'            => 'Adicionar novo',
+        'add_new_item'       => 'Adicionar novo cão',
+        'new_item'           => 'Novo cão',
+        'edit_item'          => 'Editar cão',
+        'view_item'          => 'Ver cão',
+        'all_items'          => 'Todos os cães',
+        'search_items'       => 'Buscar cães',
+        'not_found'          => 'Nenhum cão encontrado',
+        'not_found_in_trash' => 'Nenhum cão encontrado na lixeira',
+    ];
+
+    $args = [
+        'labels'             => $labels,
+        'public'             => true,
+        'show_in_rest'       => true,
+        'menu_icon'          => 'dashicons-pets',
+        'supports'           => [
+            'title',
+            'editor',
+            'thumbnail',
+            'excerpt',
+        ],
+        'has_archive'        => true,
+        'rewrite'            => [
+            'slug' => 'nossos-caes',
+        ],
+        'show_in_nav_menus'  => true,
+        'menu_position'      => 6,
+    ];
+
+    register_post_type('kadosh_dog', $args);
+}
+
+add_action('init', 'kadoshdogs_register_dogs_post_type');
